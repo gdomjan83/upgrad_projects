49753482a12b267a0cc2b61ff87b718b887dfc09
onebitstudio <gdomjan83@gmail.com>
2020-07-22 23:04:58 +0200

    First notes

---
+++ mad_libs_generator/mad_lib.py
@@ -0,0 +1,6 @@
+#1. Get console input. Nouns, pronouns, adjectives, verbs, numbers, colors etc. Should be entered in seperate lines so the correct number of words are given.
+#2. Save entered variables in a dictionary. Keys: verbs, numbers, colors etc. Values: the user inputs
+#3. Print default story template in f string format, using the inputted variables.
+
+
+
