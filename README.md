39feb40f317f179b8fac712683b677f7acfa2a73
onebitstudio <gdomjan83@gmail.com>
2020-07-30 23:03:05 +0200

    Finished logic for adventure game

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 These are the Python projects I created. The project ideas were found on the upgrad.com site.
 
 Project 1 - Number guessing game - FINISHED
+
 	This is a small guessing game. You have to guess a number between 1 and 100. If you can't guess it, you will be given hints about the number.
+
+Project 2 - Simple text adventure game - WORK IN PROGRESS
+
+	This is a small text adventure game, where the player can move through different rooms.
--- text_adventure/text_adventure.py
+++ text_adventure/text_adventure.py
@@ -9,21 +9,22 @@ for x in range(6):
     for y in range(6):
         rooms[x].append(None)
 
-rooms[2][0] = ("Room20", "u")
-rooms[0][1] = ("Room01", "r")
-rooms[1][1] = ("Room11", "rlu")
-rooms[2][1] = ("Room21", "ld")
-rooms[1][2] = ("Room12", "ud")
-rooms[0][3] = ("Room03", "r")
-rooms[1][3] = ("Room13", "dl")
-rooms[2][3] = ("Room23", "ur")
-rooms[4][3] = ("Room43", "u")
-rooms[2][4] = ("Room24", "dr")
-rooms[3][4] = ("Room34", "url")
-rooms[4][4] = ("Room44", "lrd")
-rooms[5][4] = ("Room54", "l")
-rooms[3][5] = ("Room35", "d")
+rooms[2][0] = ("Room20", "w")
+rooms[0][1] = ("Room01", "d")
+rooms[1][1] = ("Room11", "daw")
+rooms[2][1] = ("Room21", "as")
+rooms[1][2] = ("Room12", "ws")
+rooms[0][3] = ("Room03", "d")
+rooms[1][3] = ("Room13", "sd")
+rooms[2][3] = ("Room23", "wa")
+rooms[4][3] = ("Room43", "w")
+rooms[2][4] = ("Room24", "sd")
+rooms[3][4] = ("Room34", "wda")
+rooms[4][4] = ("Room44", "das")
+rooms[5][4] = ("Room54", "a")
+rooms[3][5] = ("Room35", "s")
 
+print(rooms[2][0][0])
 #set up player class as the character
 class Player():
     def __init__(self, x, y):
@@ -31,30 +32,33 @@ class Player():
         self.y = y  #the position of the player in the rooms
 
     def get_direction(self):
-        direction = input("=========================\nWhere do you want to move? (w - UP, s - DOWN, a - LEFT, d - RIGHT\n=========================")
-        if direction in rooms[self.x][self.y][1]:
-            return direction
-        else:
-            print("You can't move in that direction.")
-
+        direction = input("=========================\nWhere do you want to move? (w - UP, s - DOWN, a - LEFT, d - RIGHT, x - EXIT GAME) Enter direction: ")
+        print("=========================")
+        return direction
 
     def move(self, direction):
-        if direction == "w":
-            self.y += 1
-        elif direction == "s":
-            self.y -= 1
-        elif direction == "a":
-            self.x -= 1
-        elif direction == "d":
-            self.x += 1
-        
+        if direction in rooms[self.x][self.y][1]:
+            if direction == "w":
+                self.y += 1
+            elif direction == "s":
+                self.y -= 1
+            elif direction == "a":
+                self.x -= 1
+            elif direction == "d":
+                self.x += 1
+            print("You step through the door to the next room.")
+            print(rooms[self.x][self.y][0])
+            return (self.x, self.y)
+        else:
+            if direction == "x":
+                return False
+            print(f"You can't move in that direction.\n{rooms[self.x][self.y][0]}")      
 
+player = Player(2, 0) #set player starting position
 
-you = Player(2, 0)
-
-
-
-
-
-
-
+while True:
+    if (player.move(player.get_direction()) == False):
+        break
+    elif (player.move(player.get_direction()) == (3, 5)):
+        print("Congratulations, you reached the end of the game.")
+        break
